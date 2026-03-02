c01683c4fed24b1dd0063864b8a0fbccae84f100
Hari <amarhari26122002@gmail.com>
2026-03-02 10:09:59 +0530

    Home page is created  (GET METHOD);

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,13 @@ const http = require('http'); //  getting http module form the node
 const port = 8000;
 const server = http.createServer((req,res)=>{ // creating the server
 
-  res.end("the server is runing in port 8000 (local-host)");
+  
+    if(req.url==="/" && req.method==="GET"){
+
+     res.end("this is home page");
+
+    }
+    
 
 });
 server.listen(port,()=>{     // running the server 
