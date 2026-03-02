223e07758575901deb65d20b06d6069fd647ffc7
Hari <amarhari26122002@gmail.com>
2026-03-02 10:03:29 +0530

    comments added;

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const http = require('http'); //  getting http module form the node
 const port = 8000;
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{ // creating the server
 
   res.end("the server is runing in port 8000 (local-host)");
 
 });
-server.listen(port,()=>{
+server.listen(port,()=>{     // running the server 
 
  console.log("the server is running")
 
